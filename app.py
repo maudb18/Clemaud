16025b765f65cb9e11d370db767e1005093a0765
Maud <m-aud.b@live.fr>
2023-05-26 10:16:38 +0200

    1er fichier

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify
+import psycopg2
+
+app = Flask(__name__)
+
+# Configuration de la base de données
+db_host = 'clem-maud.postgres.database.azure.com'
+db_port = '5432'
+db_name = 'wtf'
+db_user = 'clemaud'
+db_password = 'greta@2023'
+
+# Route de l'API
+@app.route('/test')
+def get_insee_dept():
+    # Connexion à la base de données
+    conn = psycopg2.connect(
+        host=db_host,
+        port=db_port,
+        dbname=db_name,
+        user=db_user,
+        password=db_password
+    )
+    cursor = conn.cursor()
+
+    # Exécution de la requête SQL
+    query = "SELECT * FROM insee_dept"
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    # Fermeture de la connexion à la base de données
+    cursor.close()
+    conn.close()
+
+    # Conversion des résultats en format JSON
+    data = []
+    for result in results:
+        dept, insee, cheflieu = result
+        data.append({'DEPT': dept, 'INSEE': insee, 'CHEFLIEU': cheflieu})
+
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
